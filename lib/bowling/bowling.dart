a8bb8edb63279a70c2d45de437b883a9beb40b86
Borokoro <borokoro@gmail.com>
2022-11-07 15:08:44 +0100

    Added files and wrote main

---
+++ lib/bowling/bowling.dart
@@ -0,0 +1,16 @@
+
+import 'package:flutter/material.dart';
+
+class Bowling extends StatefulWidget {
+  const Bowling({Key? key}) : super(key: key);
+
+  @override
+  State<Bowling> createState() => _BowlingState();
+}
+
+class _BowlingState extends State<Bowling> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
