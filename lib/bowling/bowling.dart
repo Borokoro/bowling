6e8d60a3fb47b670e896f15cd4958c39f92cc99d
Borokoro <marek0cichon@gmail.com>
2022-11-18 18:17:32 +0100

    update2

--- lib/bowling/bowling.dart
+++ lib/bowling/bowling.dart
@@ -1,5 +1,6 @@
 
 import 'package:flutter/material.dart';
+import 'package:numberpicker/numberpicker.dart';
 
 class Bowling extends StatefulWidget {
   const Bowling({Key? key}) : super(key: key);
@@ -9,6 +10,35 @@ class Bowling extends StatefulWidget {
 }
 
 class _BowlingState extends State<Bowling> {
+  List<int> scores=[];
+  List<int> sumScore=[];
+  @override
+  void initState() {
+    for(int i=0;i<10;i++){
+      scores.add(0);
+      sumScore.add(0);
+    }
+    super.initState();
+  }
+  Widget pickScore(int index){
+    return (
+      NumberPicker(
+          minValue: 0,
+          maxValue: 10,
+          value: scores[index],
+          onChanged: (value) => setState(() => scores[index] = value),
+      )
+    );
+  }
+  MaterialAccentColor whichColor(int index){
+    if(sumScore[index]==0)
+      return Colors.indigoAccent;
+    else if(sumScore[index]<30)
+      return Colors.yellowAccent;
+    else
+      return Colors.greenAccent;
+
+  }
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -20,10 +50,44 @@ class _BowlingState extends State<Bowling> {
             begin: Alignment.bottomLeft,
             end: Alignment.topRight,
             colors: [
-              Colors.blue,
+              Colors.deepPurple,
               Colors.white,
             ],
           ),
+        ),
+        child: ListView(
+          children: [
+            for(int i=0; i<9; i++)
+              Column(
+                children: [
+                  Container(
+                    width: MediaQuery.of(context).size.width/2,
+                    height: MediaQuery.of(context).size.height/3,
+                    decoration: BoxDecoration(
+                      borderRadius: const BorderRadius.all(Radius.circular(15)),
+                      color: whichColor(i),
+                      border: Border.all(
+                        width: 5,
+                        color: Colors.white,
+                      ),
+                    ),
+                    child: Column(
+                      children: [
+                        Row(
+                          children: [],
+                        ),
+                        Container(
+                          //biała linia oddzielająca
+                        ),
+                      ],
+                    ),
+                  ),
+                  SizedBox(
+                    height: 10,
+                  )
+                ],
+              ),
+          ],
         ),
       ),
     );
